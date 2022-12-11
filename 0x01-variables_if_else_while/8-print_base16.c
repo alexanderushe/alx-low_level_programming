a5ad8e39025c89e180644b35d9f7c055d84bd885
alexanderushe <alex.f.ushe@gmail.com>
2022-12-10 19:19:20 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main
+ * Return: 0
+ */
+int main(void)
+{
+	char w;
+
+	for (w = '0'; w <= '9'; w++)
+	{
+		putchar(w);
+	}
+	for (w = 'a'; w <= 'f'; w++)
+	{
+		putchar(w);
+	}
+	putchar('\n');
+	return (0);
