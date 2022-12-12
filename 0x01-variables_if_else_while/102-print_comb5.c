de90d14155afd5a404461f53ee8c56fe558c7f21
alexanderushe <alex.f.ushe@gmail.com>
2022-12-12 07:48:04 -0800

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0 
+ */
+int main(void)
+{
+	int c = 0;
+	int a_b;
+	int b_b;
+
+	int c2;
+	int a_b2;
+	int b_b2;
+
+	while (c <= 98)
+	{
+		a_b = (c / 10 + '0');
+		b_b = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			a_b2 = (c2 / 10 + '0');
+			b_b2 = (c2 % 10 + '0');
+
+			if (c < c2)
+			{
+				putchar(a_b);
+				putchar(b_b);
+				putchar(' ');
+				putchar(a_b2);
+				putchar(b_b2;
+
+				if (c != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c2++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
