4c4a4245e71315d0a63ef57a6a56b62aa79013ce
alexanderushe <alex.f.ushe@gmail.com>
2022-12-14 03:50:19 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a < 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
