254ba564fcce3e88f1591b1d41d22cc1fc0e9ce7
alexanderushe <alex.f.ushe@gmail.com>
2022-12-19 04:59:48 -0800

    betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,8 +8,13 @@
 
 int _strlen(char *s)
 {
-	int index;
+	int i = 1, sum = 0;
+	char pl = s[0];
 
-	for (index = 0; s[index] != '\0'; index++)
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
 		return (index);
 }
