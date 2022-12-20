58d3349c9aab41e68abb1dc263e37647e91dc22d
alexanderushe <alex.f.ushe@gmail.com>
2022-12-21 00:25:36 +0200

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints elements of an array of integers
+ * @a: first pointer
+ * @b: second pointer
+ * Return: void
+ */
+
+void print_array(int *a, int b)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+		printf("\n");
+}
