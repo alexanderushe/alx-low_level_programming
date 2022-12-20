ee529a91d0c93044197eee042e99c364b86a334b
alexanderushe <alex.f.ushe@gmail.com>
2022-12-19 19:23:14 -0800

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_rev - prints string in revere
  * @s: pointer to string
@@ -10,7 +10,7 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while(s[i])
+	while (s[i])
 	{
 		i++;
 
