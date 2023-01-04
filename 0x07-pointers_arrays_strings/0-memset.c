d96f82f19e15fa6d0eb29b17be27d530b0bbb1ef
alexanderushe <alex.f.ushe@gmail.com>
2023-01-03 18:37:52 -0800

    07 memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#inlcude "main.h"
+
+/**
+ * _memset - entry point
+ * @s: pointed dest
+ * @b: constant byte
+ * @n: the bytes
+ * Return:0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	fir (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
