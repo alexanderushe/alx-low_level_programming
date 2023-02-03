dab0978cc7e3714dfda1312ce049b5b7ab8c3e10
alexanderushe <alex.f.ushe@gmail.com>
2023-02-03 12:29:10 -0800

    bits

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+/**
+ * looped_listint_count - counts the number of unique nodes
+ * @head: a pointer to the head of the listint_t to check
+ *
+ * Return: if the list is not looped - 0.
+ * Othersie the number of unique in the list
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (hare->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
+
